f751df5c4761eb64fcd31c5b131ec0b79d151ca9
Uliana Gritchina <ugritchina@icloud.com>
2022-12-14 14:58:42 +0300

    Initial Commit

---
+++ UniversePro/UniverseProApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniverseProApp.swift
+//  UniversePro
+//
+//  Created by Ульяна Гритчина on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct UniverseProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
